59def095c4182118cddcaa5d4872c3c48f233627
Djedje SEWOVON <jesssewovon@gmail.com>
2024-12-12 17:45:18 +0000

    Updates

---
+++ transfer.php
@@ -0,0 +1,39 @@
+<?php
+
+	require __DIR__ . '/vendor/autoload.php';
+
+    use Soneso\StellarSDK\Crypto\KeyPair;
+    use Soneso\StellarSDK\Util\FriendBot;
+    use Soneso\StellarSDK\StellarSDK;
+    use Soneso\StellarSDK\CreateAccountOperationBuilder;
+    use Soneso\StellarSDK\TransactionBuilder;
+    use Soneso\StellarSDK\PaymentOperationBuilder;
+    use Soneso\StellarSDK\Network;
+    /////////////ACCOUNT A SENDER////////////////////////////////////
+    //GAIMLZBSO6C5CDRJUY2RRUNW64W7BGJ5R43HMJBCOC6PN4XKHRSQF5SF
+    //SCHVLLKZYOQBQZI2IGKT6VWMJ2GLSVUJZRAOWZ2JUGIKH2VAXFUIPW3E
+
+    /////////////ACCOUNT B RECEIVER////////////////////////////////////
+    //GAHCOAISSHVZ4KK5L4GKWGAEZ2G6MOI2JOMQRMO5SSCDJDZBFF7YMEZS
+    //SD33TNJGE4WJ6LJS4D6B33HM64EMUZ2KIUIHER2Q3O4R33ANDQXWKHXI
+
+	$senderKeyPair = KeyPair::fromSeed("SCHVLLKZYOQBQZI2IGKT6VWMJ2GLSVUJZRAOWZ2JUGIKH2VAXFUIPW3E");
+	$destination = "GAHCOAISSHVZ4KK5L4GKWGAEZ2G6MOI2JOMQRMO5SSCDJDZBFF7YMEZS";
+
+	// Load sender account data from the stellar network.
+	$sender = $sdk->requestAccount($senderKeyPair->getAccountId());
+
+	// Build the transaction to send 100 XLM native payment from sender to destination
+	$paymentOperation = (new PaymentOperationBuilder($destination,Asset::native(), "100"))->build();
+	$transaction = (new TransactionBuilder($sender))->addOperation($paymentOperation)->build();
+
+	// Sign the transaction with the sender's key pair.
+	$transaction->sign($senderKeyPair, Network::testnet());
+
+	// Submit the transaction to the stellar network.
+	$response = $sdk->submitTransaction($transaction);
+	if ($response->isSuccessful()) {
+	    print(PHP_EOL."Payment sent");
+	}
+
+?>
