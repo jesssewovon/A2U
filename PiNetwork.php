3490488317da6363243c963d13a3539d907e4630
Djedje SEWOVON <jesssewovon@gmail.com>
2024-12-12 21:21:03 +0000

    Updates

---
+++ PiNetwork.php
@@ -0,0 +1,46 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Soneso\StellarSDK\Crypto\KeyPair;
+use Soneso\StellarSDK\Util\FriendBot;
+use Soneso\StellarSDK\StellarSDK;
+use Soneso\StellarSDK\Asset;
+
+use GuzzleHttp\Client;
+
+class PiNetwork{
+	private $api_key;
+	private $walletPrivateSeed;
+
+	private $httpClient;
+
+	public function __construct($api_key, $walletPrivateSeed)
+	{
+		$this->api_key = $api_key;
+		$this->walletPrivateSeed = $walletPrivateSeed;
+
+		$this->httpClient = new Client([
+            'base_uri' => "https://api.testnet.minepi.com",
+            'exceptions' => false,
+            , 'verify' => false
+        ]);
+	}
+
+	public function createPayment($paymentData)
+	{
+		$rep = $this->httpClient->request('POST', '/v2/payments', [
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Key '.$this->api_key
+            ],
+            'body' => $paymentData
+        ]);
+        $body = $rep->getBody();
+        Log::info("[body] $body");
+        $body_obj = json_decode($body, false, 512, JSON_UNESCAPED_UNICODE);
+        return $body_obj;
+	}
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,34 @@
+<?php
+
+	require __DIR__ . '/vendor/autoload.php';
+
+    use Soneso\StellarSDK\Crypto\KeyPair;
+    use Soneso\StellarSDK\Util\FriendBot;
+    use Soneso\StellarSDK\StellarSDK;
+    use Soneso\StellarSDK\CreateAccountOperationBuilder;
+    use Soneso\StellarSDK\TransactionBuilder;
+    use Soneso\StellarSDK\PaymentOperationBuilder;
+    use Soneso\StellarSDK\Network;
+    use Soneso\StellarSDK\Asset;
+    use PiNetwork;
+
+    /////////////////////////PI NETWORK////////////////////////
+    /////////////ACCOUNT A SENDER////////////////////////////////////
+    //GCXMPLIIYF264A3Q6KU4ZY2XBEPNUUHXVFC3YBVJ3FRF46R7KHAKIS6P
+    //SDORQ45BZ4626TJTQ2DDGDIWQDVK56IO6V6ZJWOEIMOG4C3RNZHF3ZUG
+
+    $api_key = "r3sw4kk0z0e3aowvpmckqnvuvyiyeuqtfraoubyngjqqrytjdzjudlk4ad5va2ve";
+    $seed = "SDORQ45BZ4626TJTQ2DDGDIWQDVK56IO6V6ZJWOEIMOG4C3RNZHF3ZUG";
+
+    $pi = new PiNetwork($api_key, $seed);
+    $paymentData = [
+        "amount": 1,
+        "memo": "Refund for apple pie", // this is just an example
+        "metadata": ["productId": "apple-pie-1"],
+        "uid": "7b568abd-31fc-49c8-bd29-ec0cebfff52e"//Filano uid
+    ];
+    $res = $pi->createPayment($paymentData);
+    echo "success";echo nl2br("\n");
+    var_dump($res);
+
+?>
