1cbf858062c4833a0fbd813517cd3fc64fadd702
Djedje SEWOVON <jesssewovon@gmail.com>
2024-12-12 22:33:20 +0000

    Updates

--- PiNetwork.php
+++ PiNetwork.php
@@ -34,7 +34,7 @@ class PiNetwork{
                 'Accept' => 'application/json',
                 'Authorization' => 'Key '.$this->api_key
             ],
-            'query' => json_encode($paymentData)
+            'query' => $paymentData
         ]);
         $body = $rep->getBody();
         Log::info("[body] $body");
