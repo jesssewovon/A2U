26672c3b8a73bd7604f76307e48c2373ba1364cd
Djedje SEWOVON <jesssewovon@gmail.com>
2024-12-13 01:11:08 +0000

    Updates

--- account-balance.php
+++ account-balance.php
@@ -8,8 +8,11 @@
     use Soneso\StellarSDK\Asset;
 
 	$accountId = "GAIMLZBSO6C5CDRJUY2RRUNW64W7BGJ5R43HMJBCOC6PN4XKHRSQF5SF";
+	$accountId = "GCXMPLIIYF264A3Q6KU4ZY2XBEPNUUHXVFC3YBVJ3FRF46R7KHAKIS6P";//PI
 
-	$sdk = StellarSDK::getTestNetInstance();
+	//$sdk = StellarSDK::getTestNetInstance();
+	$url = "https://api.testnet.minepi.com";
+    $sdk = new StellarSDK($url);
 	// Request the account data.
 	$account = $sdk->requestAccount($accountId);
 
