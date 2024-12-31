4aa40b6c9899c357c7131b61d9870188578ef710
Djedje SEWOVON <jesssewovon@gmail.com>
2024-12-31 02:40:06 +0000

    init

--- test.php
+++ test.php
@@ -37,7 +37,6 @@
                 "uid" => $uid
             ],
         ];
-        //$identifier = "wcYQr8iuxsLISyvXalhS7VYocpDV";
         $identifier = $pi->createPayment($paymentData);
     }
     $payment = $pi->getPayment($identifier);
@@ -50,10 +49,9 @@
         echo nl2br("\n");
         var_dump($submitResponse);
         $txid = $submitResponse;
-        //3dd38f3c66ad66a2171eea7c862ea1bd51e6edee61435e4faf8ea32a1bbb0a33
     } catch (\Exception $e) {
-        var_dump($e);
-        //echo "errrr : ".$e->getMessage();
+        $data = json_decode($e->getMessage());
+        $txid = $data->txid;
     }
     echo nl2br("\n");echo nl2br("\n");
     echo "Payment completion";echo nl2br("\n");
