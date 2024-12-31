97fb8464e1e6efbd0e820e0810e80dcd5a6d7e2e
Djedje SEWOVON <jesssewovon@gmail.com>
2024-12-31 02:57:07 +0000

    init

--- test.php
+++ test.php
@@ -12,12 +12,7 @@
     $api_key = "r3sw4kk0z0e3aowvpmckqnvuvyiyeuqtfraoubyngjqqrytjdzjudlk4ad5va2ve";
     $seed = "SDORQ45BZ4626TJTQ2DDGDIWQDVK56IO6V6ZJWOEIMOG4C3RNZHF3ZUG";
     $uid = "7b568abd-31fc-49c8-bd29-ec0cebfff52e";//Filano uid
-
-    //marketplace-a52471361fb46943
-    //$api_key = "awnvszy2pwdfey6jcnj7qye6yamaz5shtvqeaomsjhbazaqjadd2qeibvvaog8re";
-    //$seed = "SCDEDMODCIMJ3ISYYAWW3MXQGXCZDL4MUKYKLUADHQDHSLZPK6NK43EA";
-    $uid = "ae69e3dc-ab1d-444e-81e5-89c140a8ba56";//Moi
-    //$uid = "6ec29c65-351b-4ad9-9bcf-9760187c0242";//Filano
+    $uid = "0e2a389b-8086-45a4-9ac0-c783e0fac907";//Mon uid
 
     $pi = new PiNetwork($api_key, $seed);
     $incompletePayments = $pi->incompletePayments();
