2d08eec67c43eb90dcb0315186f2b4138cd0f9e4
Djedje SEWOVON <jesssewovon@gmail.com>
2024-12-31 01:00:45 +0000

    Updates

--- test.php
+++ test.php
@@ -31,32 +31,35 @@
     //$uid = "6ec29c65-351b-4ad9-9bcf-9760187c0242";//Filano
 
     $pi = new PiNetwork($api_key, $seed);
-    $paymentData = [
-        "payment" => [
-            "amount" => 0.42,
-            "memo" => "Refund for apple pie", // this is just an example
-            "metadata" => ["productId" => "apple-pie-1"],
-            "uid" => $uid
-        ],
-    ];
-    //$identifier = "wcYQr8iuxsLISyvXalhS7VYocpDV";
-    $paymentData = $pi->createPayment($paymentData);
-    $identifier = $paymentData->identifier;
+    $incompletePayments = $pi->incompletePayments();
+    $identifier = "";
+    $cancelAllIncompletePayments = true;
+    if ($cancelAllIncompletePayments) {
+        $pi->cancelAllIncompletePayments();
+    }elseif(is_array($incompletePayments) && isset($incompletePayments[0])){
+        $identifier = $incompletePayments[0]->identifier;
+    }
+    if($cancelAllIncompletePayments){
+        $paymentData = [
+            "payment" => [
+                "amount" => 0.42,
+                "memo" => "Refund for apple pie", // this is just an example
+                "metadata" => ["productId" => "apple-pie-1"],
+                "uid" => $uid
+            ],
+        ];
+        //$identifier = "wcYQr8iuxsLISyvXalhS7VYocpDV";
+        $paymentData = $pi->createPayment($paymentData);
+        $identifier = $paymentData->identifier;
+    }
     $payment = $pi->getPayment($identifier);
-    $identifier = $payment->identifier;
     echo "success";echo nl2br("\n");
     var_dump($payment);echo nl2br("\n");echo nl2br("\n");
-
-    $url = "https://api.testnet.minepi.com";
-    $sdk = new StellarSDK($url);
-    $response = $sdk->requestFeeStats();
-    //$feeCharged = $response->getFeeCharged();
-    $feeCharged = $response->getLastLedgerBaseFee();
-    var_dump($feeCharged);echo nl2br("\n");echo nl2br("\n");
 
     $txid = "";
     try {
         $submitResponse = $pi->submitPayment($identifier);
+        echo nl2br("\n");
         var_dump($submitResponse);
         $txid = $submitResponse;
         //3dd38f3c66ad66a2171eea7c862ea1bd51e6edee61435e4faf8ea32a1bbb0a33
@@ -64,9 +67,8 @@
         var_dump($e);
         //echo "errrr : ".$e->getMessage();
     }
-
     echo nl2br("\n");echo nl2br("\n");
-    echo "Payment completion";
+    echo "Payment completion";echo nl2br("\n");
     $paymentComplete = $pi->completePayment($identifier, $txid);
     var_dump($paymentComplete);
 
